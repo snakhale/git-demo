062922e342fdcb0b48e3ffdb3dc32bde1442fcca
snakhale <samir.nakhale@gmail.com>
2022-08-20 16:06:20 +0530

    added if condition to check data

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -1,5 +1,9 @@
 package com.javatechie.app.git_demo;
 
+import java.sql.DatabaseMetaData;
+
+import javax.xml.crypto.Data;
+
 /**
  * Hello world!
  *
@@ -9,10 +13,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+       
     }
     
     public void addData(String data) {
     	System.out.println("data ="+data);
+    	if (data.equals("Samir")) {
+    		System.out.println("I am Samir");
+    	}
     	
     }
 }
