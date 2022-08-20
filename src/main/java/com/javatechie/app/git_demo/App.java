39d76f67075cb5b401357940120427622900322c
snakhale <samir.nakhale@gmail.com>
2022-08-20 15:45:54 +0530

    added git-demo project to local reposotory

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,18 @@
+package com.javatechie.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void addData(String data) {
+    	System.out.println("data ="+data);
+    	
+    }
+}
